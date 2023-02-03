9b2618e409e7694cf941cba78640d795c46a597c
Boppafloppaloppagus <37567720+Boppafloppaloppagus@users.noreply.github.com>
2023-02-02 21:19:22 -0500

    Create TileLayer.cs
    -Added TileLayer.cs

---
+++ GGJ 2023 Project/Assets/Scripts/TileLayer.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileLayer : MonoBehaviour
+{
+    public Tile tileImage;
+    public Tilemap tileMap;
+    Vector3Int tilePos;
+    public int gridSizeX;
+    public int gridSizeY;
+    public int offsetX;
+    public int offsetY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //mappyMcMap.SetTile(new Vector3Int(0,0,0), theOne);
+        LeTiles(gridSizeX,gridSizeY);
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LeTiles(int xAxis, int yAxis)
+    {
+        for (int y = -offsetY; y < yAxis -offsetY; y++)
+        {
+            tilePos = new Vector3Int(tilePos.x, y, tilePos.z);
+            for (int x = -offsetX; x < xAxis - offsetX; x++)
+            {
+                tilePos = new Vector3Int(x, tilePos.y, tilePos.z);
+                tileMap.SetTile(new Vector3Int(tilePos.x, tilePos.y, tilePos.z), tileImage);
+            }
+        }
+    }
+}
