d1f224800425d974bbdc45aecc781887f170c1ba
Jonathan Moriarty <jonathan.taylor.moriarty@gmail.com>
2023-02-04 12:25:07 -0500

    Added ability to swap layers with arrow keys.

--- GGJ 2023 Project/Assets/Scripts/Gameplay/GameController.cs
+++ GGJ 2023 Project/Assets/Scripts/Gameplay/GameController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
-
+using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
 
 public class GameController : MonoBehaviour
 {
@@ -15,6 +16,21 @@ public class GameController : MonoBehaviour
     public double startingEnergy = 100;
     public double energyDecay = 0.1;
 
+    public enum TilemapLayer { Surface = 2, Root = 1, Base = 0 }
+
+    public TilemapLayer startingLayer = TilemapLayer.Surface;
+
+    public TilemapLayer CurrentLayer { get; set; }
+
+    [SerializeField]
+    private Tilemap[] surfaceTilemaps;
+
+    [SerializeField]
+    private Tilemap[] rootTilemaps;
+
+    [SerializeField]
+    private Tilemap[] baseTilemaps;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,9 +40,26 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Update state based on player input.
+
+        // Change the active later.
+        var keyboard = Keyboard.current;
+
+        if (keyboard.upArrowKey.wasPressedThisFrame && CurrentLayer < TilemapLayer.Surface)
+        {
+            //CurrentLayer++;
+            SetActiveLayer(++CurrentLayer);
+        }
+        else if (keyboard.downArrowKey.wasPressedThisFrame && CurrentLayer > TilemapLayer.Base)
+        {
+            //CurrentLayer--;
+            SetActiveLayer(--CurrentLayer);
+        }
+        //SetActiveLayer(CurrentLayer);
+
         Energy -= energyDecay * Time.deltaTime;
 
-        energyText.text = $"Energy: {Energy}";
+        energyText.text = $"Energy: {Energy.ToString(".02")}";
         if (Energy <= 0) {
             // Game over
             // TODO: more elaborate sequence or animation, for now just scene change.
@@ -34,7 +67,33 @@ public class GameController : MonoBehaviour
         }
     }
 
+    void SetActiveLayer (TilemapLayer newLayer)
+    {
+        for (int i = 0; i < surfaceTilemaps.Length; i++)
+        {
+            Color newColor = surfaceTilemaps[i].color;
+            newColor.a = newLayer == TilemapLayer.Surface ? 1f : 0f;
+            surfaceTilemaps[i].color = newColor;
+        }
+
+        for (int i = 0; i < rootTilemaps.Length; i++)
+        {
+            Color newColor = rootTilemaps[i].color;
+            newColor.a = newLayer == TilemapLayer.Root ? 1f : 0f;
+            rootTilemaps[i].color = newColor;
+        }
+
+        for (int i = 0; i < baseTilemaps.Length; i++)
+        {
+            Color newColor = baseTilemaps[i].color;
+            newColor.a = newLayer == TilemapLayer.Base ? 1f : 0f;
+            baseTilemaps[i].color = newColor;
+        }
+    }
+
     void StartGame() {
         Energy = startingEnergy;
+        CurrentLayer = startingLayer;
+        SetActiveLayer(CurrentLayer);
     }
 }
